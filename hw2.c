271dd64ea1ab1e5d25fc93e5e84a0b67c9d2a90f
SihyuniS <145095415+SihyuniS@users.noreply.github.com>
2023-09-27 14:58:02 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	double km, mile;
+	printf("Please enter kilometers: ");
+	scanf_s("%lf", &km);
+
+	mile = km / 1.609;
+
+	printf("%.1lf km is equal to %.1lf miles\n", km, mile);
+
+	return 0;
+}
