20a81d1c5e50d7eec8a146f590fa1dd7e6484957
시현 <145095415+SihyuniS@users.noreply.github.com>
2023-10-19 16:36:53 +0900

    Add files via upload

---
+++ hw5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void solve(int n) 
+{
+    if (n == 0 || n == 1)
+        printf("%d", n);
+    else
+    { 
+        solve(n / 2);
+        printf("%d", n % 2);
+    } 
+}
+
+int main(void) 
+{
+    int num;
+
+    printf("Please enter a number: ");
+    scanf_s("%d", &num);
+
+    solve(num);
+
+    return 0;
+}
